02256ec2417dda3844879b18ccf1d23eca32a991
devtyper <92617941+devtyper@users.noreply.github.com>
2022-08-22 21:58:14 -0700

    Add laser

--- dodge-the-creeps/Player.gd
+++ dodge-the-creeps/Player.gd
@@ -4,15 +4,17 @@ signal hit
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+export var angle : float
 
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
+	#hide()
 
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	var array : PoolVector2Array
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -24,6 +26,7 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		angle = velocity.angle()
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
@@ -40,9 +43,13 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+	if Input.is_action_pressed("shoot_laser"):
+		#array = [position,position+Vector2(0,100)]
+		array = [Vector2(0,0),Vector2(1000*cos(angle),1000*sin(angle))]
+		$Laser.set_points(array)
 
 
-func _on_Player_body_entered(body):
+func _on_Player_body_entered(_body):
 	hide() # Player disappears after being hit.
 	emit_signal("hit")
 	# Must be deferred as we can't change physics properties on a physics callback.
